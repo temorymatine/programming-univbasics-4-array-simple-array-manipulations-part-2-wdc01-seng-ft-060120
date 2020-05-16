3001d0c7a420c3e8584edb9ea15771c745fba2e2
Matine temory <temorymatine@gmail.com>
2020-05-16 19:31:52 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,8 @@
+def using_concat(array,num)
+ @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+    @all_my_favs = @my_favorite_things.concat(more_favs)
+end
+def using_insert(array)
+  
+  
