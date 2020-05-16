0d975512e2b6d7ed8c6e3fcee0c5eecd5eb6d5e4
Matine temory <temorymatine@gmail.com>
2020-05-16 19:35:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,9 +3,9 @@ def using_concat(array,num)
     more_favs = ["sports cars", "flatiron school"]
     @all_my_favs = @my_favorite_things.concat(more_favs)
 end
-def using_insert(array)
+def using_insert(array,number)
   
   @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
     @another_language = "Python"
-    @new_array =  @list_of_programming_languages.insert(2, @another_language)
+    @new_array =  @list_of_programming_languages.insert(4, @another_language)
   end
